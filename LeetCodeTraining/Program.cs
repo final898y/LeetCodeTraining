5e000f77cf3cfecd305cfc951cf76e7a55fa1b33
BuildTools <unconfigured@null.spigotmc.org>
2024-06-10 22:19:32 +0800

    加入專案檔案。

---
+++ LeetCodeTraining/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2133. Check if Every Row and Column Contains All Numbers
+//https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/description/
+
+
+using leetcode2133;
+
+int[] test = { 1, 2, 3, 3, 5, 6 };
+Console.WriteLine(false_ex.CheckValidtest(test));
+int[][] matrix =
+        [
+            [1, 2, 3],
+            [3, 1, 2],
+            [3, 3, 1],
+        ];
+Console.WriteLine(matrix.Length);
+Console.WriteLine(matrix[2][1]);
+Console.WriteLine(false_ex.CheckValid(matrix));
+
+
+
---
+++ LeetCodeTraining/false_ex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2133
+{
+    internal class false_ex
+{
+        //失敗 使用sum不行
+        public static bool CheckValid(int[][] matrix)
+        {
+            //固定列，取得個別列的sum
+            int[] sum_fixed_i = new int[matrix.Length];
+
+            int[] sum_fixed_j = new int[matrix.Length];
+            int checksum = 0;
+            for (int i = 1; i <= matrix.Length; i++)
+            {
+                checksum += i;
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    sum_fixed_i[j] += matrix[i][j];
+                    sum_fixed_j[i] += matrix[i][j];
+                }
+            }
+            int[] combinedArray = sum_fixed_i.Concat(sum_fixed_j).ToArray();
+            foreach (int i in combinedArray)
+            {
+                if (i == checksum)
+                {
+                    continue;
+                }
+                return (false);
+            }
+            return (true);
+        }
+
+
+
+        public static bool CheckValidtest(int[] matrixtest)
+        {
+            for (int i = 0; i < matrixtest.Length - 1; i++)
+            {
+                Array.Sort(matrixtest);
+                if (matrixtest[i] == i + 1)
+                {
+                    if (i == matrixtest.Length - 1 & matrixtest[i] == i + 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    continue;
+
+                }
+                Console.WriteLine($"fales1_{i}");
+
+                return (false);
+            }
+            return (true);
+
+        }
+    }
+}
