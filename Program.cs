958b093ff7525737475eabaeefe2b26d46888b2a
chun chen lai <pcwinalan@outlook.com>
2024-10-27 14:34:45 +0800

    refactor.整理架構

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetcodeTraining;
+
+//2037. Minimum Number of Moves to Seat Everyone
+int[] seats = [3, 1, 5];
+int[] students = [2, 7, 4];
+Console.WriteLine(AcceptedBefore202410.minMovesToSeat(seats, students));
+
+//2133. Check if Every Row and Column Contains All Numbers
+int[][] matrix2133 =
+        [
+            [1, 2, 3],
+            [3, 1, 2],
+            [3, 3, 1],
+        ];
+Console.WriteLine(AcceptedBefore202410.CheckValid(matrix2133));
+
